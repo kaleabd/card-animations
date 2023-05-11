3bc7bf63d8d485b311f08fc99bfd8f42afd56afd
kaleabd <kaleabdereje94@gmail.com>
2023-05-11 07:43:33 +0300

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { blogList } from "./assets/data.js";
 import Card from "./components/Card.js";
 
 export default function App() {
-  return blogList.map((item, index) => (
-      <div className=" flex justify-center items-center my-[14rem]" key={index}>
-          <Card image={item.image} h2={item.h2} p={item.p}  />
-      </div>
-  ));
+  return(
+    <div>
+      {
+        blogList.map((item, index) => (
+          <div className=" flex justify-center items-center my-[14rem]" key={index}>
+              <Card image={item.image} h2={item.h2} p={item.p}  />
+          </div>
+        ))
+      }
+    </div>
+  )
 }
